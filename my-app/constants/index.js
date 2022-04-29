d9b876363ed07dadb4052b82fd8829310eea0ea6
rsergyu <94016386+rsergyu@users.noreply.github.com>
2022-04-29 13:03:22 +0300

    Update index.js

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -51,5 +51,5 @@ export const abi =[
 		"stateMutability": "nonpayable",
 		"type": "function"
 	}
-]
-export const NFT_CONTRACT_ADDRESS = 0x4519Bb1CDFd45211071ca2a254866c98C86605e4
+];
+export const NFT_CONTRACT_ADDRESS = "0x4519Bb1CDFd45211071ca2a254866c98C86605e4";
